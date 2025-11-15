a08272594327634db3b495cfb7d1328917a0f156
Rajesh Kodaganti (Rugved) <kodagantir295@gmail.com>
2025-11-14 16:09:47 -0800

    fix: tailwind ESM import and remove legacy duplicate root files

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,3 +1,5 @@
+import typography from '@tailwindcss/typography';
+
 /** @type {import('tailwindcss').Config} */
 export default {
   content: [
@@ -7,7 +9,5 @@ export default {
   theme: {
     extend: {},
   },
-  plugins: [
-    require('@tailwindcss/typography'),
-  ],
+  plugins: [typography],
 };
